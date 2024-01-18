0fee50a631972e1b896fcaa0a21d0831c4537a6d
astro <angelica.strong0725@gmail.com>
2024-01-18 00:03:14 -0800

    set api key and variables from dom

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+const apiKey = 'ad0451d039549dcdec87e21b8af0b295';
+
+const searchButton = document.getElementById('search-button');
+const cityInput = document.getElementById('city-input');
+const cityListDiv = document.getElementById('city-list');
+const currentWeatherDiv = document.getElementById('current-weather');
+const forecastCardsDiv = document.getElementById('forecast-cards');
